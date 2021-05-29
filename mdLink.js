3b4f52d60010615a2448d65c8e9dcd4deb2d4dbb
k-garrido <katherinne.g@hotmail.com>
2021-05-29 13:39:39 -0500

    Desarrollo de funcion recursiva y la funcion para convertir rutas

--- mdLink.js
+++ mdLink.js
@@ -2,20 +2,31 @@ const fs = require('fs')
 const argv = process.argv.slice(2)
 const pathModule = require('path')
 
-// Comprobando si es un directorio o un archivo directo
+
+let finalPath = ''
+
+// Comprobando si es un directorio o un archivo directo.
 const searchMD = (path) => {
-  const stat = fs.lstatSync(path).isDirectory()
-  if (stat) {
-    const openFile = fs.readdirSync(path)
-    openFile.forEach ((file)=>{
-      const filesDirectory = pathModule.join(path, file); // convierte el array en string
-      console.log(filesDirectory);
+  const fileStats = fs.lstatSync(path).isDirectory();
+  if (fileStats) {
+    const readingDir = fs.readdirSync(path);
+    readingDir.forEach((file) => {
+      if (pathModule.extname(file) === '.md') {
+        const fullPath1 = pathModule.join(path, file);
+        finalPath = fullPath1;
+      } else {
+        const fullPath2 = pathModule.join(path, file);
+        searchMD(fullPath2)
+      }
     })
-
-     return console.log (openFile)
+  } else if (pathModule.extname(path) === '.md') {
+    finalPath = path;
+  } else if (finalPath === '') {
+    console.log('La ruta ingresada no direcciona a un archivo .md');
   }
- 
-}
+  return finalPath;
+};
+searchMD(argv[0]);
 
-searchMD(argv[0])
-
+// Convirtiendo la ruta relativa a ruta absoluta
+const absoluteRoute = pathModule.resolve(finalPath)
